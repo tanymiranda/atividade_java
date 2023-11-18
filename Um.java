b3802ae239e4c13ce909cad59acdb30fb7d3f4ad
tanymiranda <143644015+tanymiranda@users.noreply.github.com>
2023-11-18 18:51:19 -0300

    Create Um.java

---
+++ Um.java
@@ -0,0 +1,10 @@
+package TodosExecicios;
+//1. Escreva um programa que imprima os números de 1a10.
+
+public class Primeiro {
+	public static void main (String[]args) {
+		for(int i =1; i<=10;i++) {
+			System.out.println(i);
+		}
+	}
+}
