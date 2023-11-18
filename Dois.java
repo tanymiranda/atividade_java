b0d524d1ee33a7c3085c076766ad07aff2fa6651
tanymiranda <143644015+tanymiranda@users.noreply.github.com>
2023-11-18 18:54:05 -0300

    Create Dois.java

---
+++ Dois.java
@@ -0,0 +1,11 @@
+package TodosExecicios;
+//2.Crieumprogramaqueimprimaosnúmerosparesde1a20.
+public class Segundo {
+	public static void main (String[]args) {
+		for(int i =1; i<=20;i++) {
+			if(i%2==0) {
+				System.out.println(i);
+			}
+		}
+	}
+}
