f924cb7b279384eb1b97c8a3894a6a7dddd77830
tanymiranda <143644015+tanymiranda@users.noreply.github.com>
2023-12-07 00:26:46 -0300

    Create Lista.java

---
+++ revisao/Lista.java
@@ -0,0 +1,23 @@
+package treinarexerc;
+//List <String> list = new ArrayList<String>();
+//list.add("Batata");
+/*List.add("Aula de java");
+* System.out.println(list.get(0));
+*///>>> Dotum;
+import java.util.ArrayList;
+import java.util.List;
+public class Lista {
+		public static void main(String [] args) {
+		List<String> list = new ArrayList<String>();
+		list.add("nome1");
+		list.add("nome2");
+		list.add("nome3");
+		list.add("nome4");
+		list.add("nome5");
+		
+		for(int i=0; i <4; i++) {
+			System.out.println(list.get(i));
+		}
+		}
+	}
+
